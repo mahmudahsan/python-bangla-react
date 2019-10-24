55cf9b6d126fed70d2a165c9b25bfc473c07d876
Mahmud Ahsan <mahmud@thinkdiff.net>
2019-10-24 22:24:09 +0800

    missing footer component added

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -17,14 +17,13 @@ export default (props) => {
           </div>
           <div className="col-sm-4">
               <p>
-              <span role="img" aria-label="developed by">💻</span> Developed by <a href={Config.social.twitter} target="_blank" rel="noreferrer noopener">{Config.author.name}</a>
+              <span role="img" aria-label="developed by">👍</span> Developed by <a href={Config.social.twitter} target="_blank" rel="noreferrer noopener">{Config.author.name}</a>
                   <br /> © {new Date().getFullYear()} <a href={Config.social.twitter} target="_blank" rel="noreferrer noopener">{Config.author.name}</a>
               </p>
           </div>
           <div className="col-sm-4">
               <p>
-                  <a href="https://github.com/mahmudahsan/python-bangla-react" target="_blank" rel="noreferrer noopener"><span role="img" aria-label="github">🧐</span>Github Source</a> <br />
-                  <a href="https://github.com/mahmudahsan/pythonbangla.com" target="_blank" rel="noreferrer noopener"><span role="img" aria-label="github">🤔</span>Original Django Source </a>
+                  <a href="https://github.com/mahmudahsan/python-bangla-react" target="_blank" rel="noreferrer noopener"><span role="img" aria-label="github">🎯</span>Open Source Project</a> <br />
               </p>
           </div>
       </div>
--- frontend/src/components/RoutingComponent.js
+++ frontend/src/components/RoutingComponent.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import Cards from './Cards';
 import Playback from './playback/Playback';
+import Footer from './Footer';
 
 const RoutingComponent = (props) => {
   return (
@@ -12,6 +13,7 @@ const RoutingComponent = (props) => {
       <Cards />
       <div className="mb-2"></div>
       <Playback topic={props.match.path} />
+      <Footer />
     </div>
   );
 };
