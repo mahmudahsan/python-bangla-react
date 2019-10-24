070f447e4928d6c1a5d01d6ba5111a1d3ba99391
Mahmud Ahsan <mahmud@thinkdiff.net>
2019-10-25 01:05:20 +0800

    if no youtube but only description is provided show that

--- frontend/src/components/playback/Youtube.js
+++ frontend/src/components/playback/Youtube.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import Linkify from 'react-linkify'
-//import Config from '../config/Settings';
+import Config from '../../config/Settings';
 
 const Youtube = props => {
   const activeVideo = props.activevideo;
@@ -13,9 +13,11 @@ const Youtube = props => {
     <div id="youtube-container" className="col-8">
       <div className="tab-content" id="nav-tabContent">
         <div className="tab-pane fade active show" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
-          <div className="videoWrapper">
-            <iframe title="python bangla" id="youtube-player-ID" type="text/html" width="560" height="315" src={"https://www.youtube.com/embed/"+activeVideo.youtube} frameBorder="0" allowFullScreen></iframe>
-          </div>
+          {activeVideo && activeVideo.youtube !== '' && (
+            <div className="videoWrapper">
+              <iframe title={Config.logoAlt} id="youtube-player-ID" type="text/html" width="560" height="315" src={"https://www.youtube.com/embed/"+activeVideo.youtube} frameBorder="0" allowFullScreen></iframe>
+            </div>
+          )}
           <div className="mb-3"></div>
           <div id="youtube-description" className="youtube-description">
             <pre style={{whiteSpace: 'pre-wrap'}}><Linkify>{activeVideo.description}</Linkify></pre>
